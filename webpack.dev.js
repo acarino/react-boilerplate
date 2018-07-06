c24c0f2edc9dd18972802b7795907b1e135c83ea
Alan Carino <alan.carino@thomsonreuters.com>
2018-07-06 14:09:58 -0400

    working react template with webpack, babel, sass, enzyme, and inital test

---
+++ webpack.dev.js
@@ -0,0 +1,13 @@
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+
+
+module.exports = merge(common, {
+	
+	mode: 'development',
+	devServer: {
+		host: 'localhost',
+		port: 3003,
+		open: true,
+	}
+})
